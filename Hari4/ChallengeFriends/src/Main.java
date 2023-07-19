261defdbaec0cd1724c8b6c92ac9833d8f6e1000
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-07-19 18:44:51 +0700

    improve challege

--- Hari4/ChallengeFriends/src/Main.java
+++ Hari4/ChallengeFriends/src/Main.java
@@ -5,28 +5,53 @@ import java.util.List;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Person person1 = new Person("Andi", List.of("Akmal", "Aurel", "Ali"));
-        Person person2 = new Person("Yuli", List.of("Akmal", "Ida", "Yuna"));
-        List<String> commonFriends = findFriends(person1, person2);
+    Person devin = new Person("Devin");
+    Person josh = new Person("Josh");
+    Person jake = new Person("Jake");
+    Person jason = new Person("Jason");
+    Person alex = new Person("Alex");
 
-        System.out.println("Teman yang sama antara " + person1.getName() + " dan " + person2.getName() + ":");
-        for (String friend : commonFriends) {
-            System.out.println(friend);
-        }
+    devin.addFriend(josh);
+    devin.addFriend(jason);
+    devin.addFriend(alex);
+    josh.addFriend(alex);
+    josh.addFriend(devin);
+
+        System.out.println(devin+" memiliki "+devin.getFriendCount() +" yaitu "+devin.getFriends());
+        System.out.println(josh+" memiliki "+josh.getFriendCount() +" yaitu "+ josh.getFriends());
+        System.out.println(josh+ " dan " +devin.getName() +" memiliki teman yang sama sebanyak "+
+                josh.getMutualFriendCount(devin) + " orang. Yaitu ");
+        josh.printFriends(josh.getMutualFriends(devin));
     }
-        public static List<String> findFriends(Person person1, Person person2){
-            List<String> commonFriends = new ArrayList<>();
-            for (String friend : person1.getFriends()){
-                if(person2.getFriends().contains(friend)){
-                    commonFriends.add(friend);
-                }
-            }
-            return commonFriends;
-        }
-
-    }
+}
 
 
-
+//import java.util.ArrayList;
+//        import java.util.List;
+//
+//// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+//// then press Enter. You can now see whitespace characters in your code.
+//public class Main {
+//    public static void main(String[] args) {
+//        Person person1 = new Person("Andi", List.of("Akmal", "Aurel", "Ali"));
+//        Person person2 = new Person("Yuli", List.of("Akmal", "Ida", "Yuna"));
+//        List<String> commonFriends = findFriends(person1, person2);
+//
+//        System.out.println("Teman yang sama antara " + person1.getName() + " dan " + person2.getName() + ":");
+//        for (String friend : commonFriends) {
+//            System.out.println(friend);
+//        }
+//    }
+//    public static List<String> findFriends(Person person1, Person person2){
+//        List<String> commonFriends = new ArrayList<>();
+//        for (String friend : person1.getFriends()){
+//            if(person2.getFriends().contains(friend)){
+//                commonFriends.add(friend);
+//            }
+//        }
+//        return commonFriends;
+//    }
+//
+//}
 
 
--- Hari4/ChallengeFriends/src/Person.java
+++ Hari4/ChallengeFriends/src/Person.java
@@ -1,17 +1,60 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Person {
     private String name;
-    private List<String> friends;
+    private ArrayList<Person> friends;
 
-    public Person(String name, List<String> friends) {
+    public Person(String name) {
         this.name = name;
-        this.friends = friends;
+        this.friends = new ArrayList<>();
     }
     public String getName(){
-        return name;
+        return this.name;
     }
-    public  List<String> getFriends (){
-        return  friends;
+    public void setName(String name) {this.name = name;}
+
+    public void addFriend(Person friend){
+       this.friends.add(friend);
+    }
+    public void removeFriend(Person person){
+        this.friends.remove(person);
+    }
+
+    public ArrayList<Person> getFriends(){
+        return this.friends;
+    }
+
+    public ArrayList<Person> getMutualFriends(Person person){
+        ArrayList<Person> friends = new ArrayList<>();
+        for (Person friend: this.friends){
+            if (person.getFriends().contains(friend)){
+                friends.add(friend);
+            }
+        }
+        return friends;
+    }
+
+    public int getFriendCount(){return this.friends.size();}
+
+    public int getMutualFriendCount(Person person){
+        int count =0;
+        for (Person friend:this.friends){
+            if (person.getFriends().contains(friend)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void  printFriends(ArrayList<Person> friends){
+        for (Person friend: friends){
+            System.out.println(friend.getName()+" ");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
