050cb65add9e26d9695156e3a6c7ebb361e7e8a4
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-07-20 20:01:26 +0700

    Robot solution refactoring

--- Hari4/src/model/Direction.java
+++ Hari4/src/model/Direction.java
@@ -4,7 +4,14 @@ public enum Direction {
     NORTH,
     EAST,
     SOUTH,
-    WEST
+    WEST;
+
+    public Direction turnLeft(){
+        return Direction.values()[(ordinal()+3)%4];
+    }
+    public Direction turnRight(){
+        return Direction.values()[(ordinal()+1)%4];
+    }
 
 
 }
