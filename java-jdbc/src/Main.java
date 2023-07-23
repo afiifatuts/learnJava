e21dce977b9eba9621136da31847c72b67b3439d
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-07-23 13:44:15 +0700

    connection java jdbc exception 1

--- java-jdbc/src/Main.java
+++ java-jdbc/src/Main.java
@@ -4,7 +4,6 @@ import java.sql.DriverManager;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    //Java JDBC
     public static void main(String[] args) {
         String url ="jdbc:postgresql://localhost:5432/toko_mandiri";
         String username = "postgres";
