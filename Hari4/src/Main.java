c6e853f26a629646f170204039a4f29a4a717d0e
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-07-23 13:45:10 +0700

    connection java jdbc exception 2

--- Hari4/src/Main.java
+++ Hari4/src/Main.java
@@ -1,20 +1,19 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     //Java JDBC
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         String url ="jdbc:postgresql://localhost:5432/toko_mandiri";
         String username = "postgres";
         String password ="blimbeng38";
-        try {
+
             Connection connection = DriverManager.getConnection(url,username,password);
             System.out.println("sucessfully connected");
-        }catch (Exception e){
-            System.out.println(e);
-        }
+
 
     }
 }
