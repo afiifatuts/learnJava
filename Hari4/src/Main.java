b71c076ed7c7026cd94a92e916e822bfa549d03c
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-07-23 20:11:05 +0700

    insert sekaligus banyak

--- Hari4/src/Main.java
+++ Hari4/src/Main.java
@@ -3,6 +3,7 @@ import model.Product;
 import repository.ProductRepo;
 
 import java.sql.*;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,26 +15,36 @@ public class Main {
         ConnectToDb connectToDb = new ConnectToDb();
         Connection connection = connectToDb.startConnection();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Username : ");
-        String username = scanner.nextLine();
-        System.out.println("Password : ");
-        String password = scanner.nextLine();
+        //menambahkan product sekaligus banyak
+        Product product1 = new Product(9000000, 6, "Laptop");
+        Product product2 = new Product(9000000, 6, "Keyboard");
+        Product product3 = new Product(900000, 6, "Monitor");
 
-//        Statement statement = connection.createStatement();
-//        ResultSet resultSet = statement.executeQuery("SELECT * FROM mst_user WHERE username = '"+username+"' and password= '"+password+"'");
-//
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM mst_user WHERE username =? and password=?");
-        preparedStatement.setString(1, username);
-        preparedStatement.setString(2, password);
-        ResultSet resultSet1 = preparedStatement.executeQuery();
+        // Find the maximum product_id in the table and increment it
+        String queryMaxProductId = "SELECT MAX(product_id) FROM mst_product";
+        Statement maxProductIdStatement = connection.createStatement();
+        ResultSet resultSet = maxProductIdStatement.executeQuery(queryMaxProductId);
+        int maxProductId = 0;
+        if (resultSet.next()) {
+            maxProductId = resultSet.getInt(1);
+        }
+        maxProductId++; // Increment the max product_id to get a new unique value
 
-        if (resultSet1.next()){
-            System.out.println("Login successfully");
-        }else{
-            System.out.println("login gagal");
+        List<Product> dataProduct = Arrays.asList(product1, product2, product3);
+
+        String queryInsertProducts = "INSERT INTO mst_product (product_id, product_name, product_price, stock) VALUES (?, ?, ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(queryInsertProducts);
+
+        for (Product p : dataProduct) {
+            preparedStatement.setInt(1, maxProductId);
+            preparedStatement.setString(2, p.getProductName());
+            preparedStatement.setInt(3, p.getProductPrice());
+            preparedStatement.setInt(4, p.getStock());
+            preparedStatement.addBatch();
+            maxProductId++; // Increment the product_id for the next iteration
         }
 
+        preparedStatement.executeBatch();
 
     }
 }
@@ -52,6 +63,27 @@ public class Main {
         Product product = productRepo.getProduct(1);
         System.out.println(product.toString());
 
+
+*
+*         Scanner scanner = new Scanner(System.in);
+        System.out.println("Username : ");
+        String username = scanner.nextLine();
+        System.out.println("Password : ");
+        String password = scanner.nextLine();
+
+//        Statement statement = connection.createStatement();
+//        ResultSet resultSet = statement.executeQuery("SELECT * FROM mst_user WHERE username = '"+username+"' and password= '"+password+"'");
+//
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM mst_user WHERE username =? and password=?");
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+        ResultSet resultSet1 = preparedStatement.executeQuery();
+
+        if (resultSet1.next()){
+            System.out.println("Login successfully");
+        }else{
+            System.out.println("login gagal");
+        }
 
 
 
